fb82bac91526f8536f07e455f0aace2ce2081053
mayank jain <mayankjain1394@gmail.com>
2025-06-29 13:18:14 +0530

    fixed scroll issue

--- frontend/src/app/scheme-overlay/scheme-overlay.component.ts
+++ frontend/src/app/scheme-overlay/scheme-overlay.component.ts
@@ -1,5 +1,6 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, output, effect, OnDestroy, inject, Renderer2, ElementRef, Inject } from '@angular/core';
 import { Scheme } from '../chat';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-scheme-overlay',
@@ -7,12 +8,34 @@ import { Scheme } from '../chat';
   templateUrl: './scheme-overlay.component.html',
   styleUrl: './scheme-overlay.component.scss'
 })
-export class SchemeOverlayComponent {
+export class SchemeOverlayComponent implements OnDestroy {
   scheme = input.required<Scheme>();
+  onOverlayClose = output<void>();
 
-  onOverlayClose = output();
+  private renderer = inject(Renderer2);
 
-  closeOverlay(): void {
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    effect(() => {
+      if (this.scheme()) {
+        this.toggleScroll(false);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.toggleScroll(true);
+  }
+
+  closeOverlay() {
+    this.toggleScroll(true);
     this.onOverlayClose.emit();
+  }
+
+  private toggleScroll(scrollValue: boolean) {
+    const chatMessagesElement = this.document.querySelector('.chat-messages') as HTMLElement;
+
+    if (chatMessagesElement) {
+      this.renderer.setStyle(chatMessagesElement, 'overflow', scrollValue ? 'auto' : 'hidden');
+    }
   }
 }
